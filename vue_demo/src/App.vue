d312b0cba139ab9a5209fe77601847032d75f8e6
刘雅欣 <liuyx1017@126.com>
2020-02-20 18:39:30 +0800

    案例-初始化

---
+++ vue_demo/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对Vue的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <Add/>
+      <List :comments="comments"/>
+    </div>
+  </div>
+</template>
+<script>
+import Add from './components/Add.vue'
+import List from './components/List.vue'
+export default {
+  data () {
+    return {
+      comments: [
+        {
+          name: 'AAA',
+          content: '想见你'
+        },
+        {
+          name: 'BBB',
+          content: '想见你'
+        },
+        {
+          name: 'CCC',
+          content: '想见你'
+        }
+      ]
+    }
+  },
+  components: {
+    Add,
+    List
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ vue_demo/src/components/List.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="col-md-8">
+    <h3 class="reply">评论回复：</h3>
+    <h2 style='display: none'>暂无评论，点击左侧添加评论！！！</h2>
+    <ul class="list-group">
+      <Item V-for="(comment,index) in comments" :key="index" :comment="comment"/>
+    </ul>
+  </div>
+</template>
+
+<script>// eslint-disable-next-line no-unused-vars
+import Item from './Item.vue'
+export default {
+  // 声明接收属性
+  props: ['comments']
+}
+</script>
+
+<style>
+  .reply {
+    margin-top: 0px;
+  }
+
+  li {
+    transition: .5s;
+    overflow: hidden;
+  }
+
+  .handle {
+    width: 40px;
+    border: 1px solid #ccc;
+    background: #fff;
+    position: absolute;
+    right: 10px;
+    top: 1px;
+    text-align: center;
+  }
+
+  .handle a {
+    display: block;
+    text-decoration: none;
+  }
+
+  .list-group-item .centence {
+    padding: 0px 50px;
+  }
+
+  .user {
+    font-size: 22px;
+  }
+
+</style>
